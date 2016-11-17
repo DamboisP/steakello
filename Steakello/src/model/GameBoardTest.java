7b76a948dcac3e0b0e6fa0a8d596d37340f99aca
jb.bonhomme <j.bonhomme@students.ephec.be>
2016-11-17 15:36:31 +0100

    Javadoc + JUnit
    Ajout de javadoc + JUnit test
    Version 1

---
+++ Steakello/src/model/GameBoardTest.java
@@ -0,0 +1,74 @@
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameBoardTest {
+
+	@Test
+	public final void testGameBoard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetChipArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetChipArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testAddChip() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSwitchPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testIsFull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetCoords() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetX() {
+		fail("Not yet implemented");
+	}
+
+}
